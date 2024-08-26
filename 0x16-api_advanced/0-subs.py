4c8ed57500cfcda68a9ef2a67696e7904b94916f
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-26 11:24:51 +0300

    returns the number of sub scripers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers to the subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-User-Agent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+    except requests.RequestException:
+        pass
+
+    return 0
