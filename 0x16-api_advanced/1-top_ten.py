6d64cfb3e1d748834ea0eed3f60db3cf528d73dd
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-26 11:50:49 +0300

    print first 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
+    if not isinstance(subreddit, str) or subreddit is None:
+        print("None")
+        return
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+
+    try:
+        response = get(url, headers=user_agent,
+                       params=params, allow_redirects=False)
+        if response.status_code == 200:
+            results = response.json()
+            posts = results.get('data', {}).get('children', [])
+            if posts:
+                for post in posts:
+                    print(post.get('data', {}).get('title', ""))
+            else:
+                print("None")
+        else:
+            print("None")
+    except Exception:
+        print("None")
+
+
+top_ten("python")  # Should print the titles of the first 10 hot posts in r/python
+top_ten("nonexistentsubreddit")  # Should print "None"
