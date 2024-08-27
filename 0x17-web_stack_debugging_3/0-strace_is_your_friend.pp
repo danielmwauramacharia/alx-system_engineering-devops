18be4ea058898d15456bf88be84a369cce58bb98
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-27 05:36:01 +0000

    fix puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #fix bad "phpp" extentions to "php" in "wp-setting.php".
-exec{'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path 	=> '/usr/local/bin/:/bin/'
+{ 'fix-wordpress':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/bin'],
 }
 
