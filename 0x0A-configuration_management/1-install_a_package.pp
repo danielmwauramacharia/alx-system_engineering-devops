1a710ffcb28b4bc5d68b2e9f5984d7138ba87411
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-27 04:41:06 -0700

    fix the puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,14 @@
 # Install Python 3.8 using apt (for Debian/Ubuntu systems)
 package { 'python3.8':
-  ensure => 'installed',
+  ensure   => 'installed',
   provider => 'apt',
 }
 
 # Install pip3 (package manager for Python3)
 package { 'python3-pip':
-  ensure => 'installed',
+  ensure   => 'installed',
   provider => 'apt',
-  require => Package['python3.8'],
+  require  => Package['python3.8'],
 }
 
 # Install Flask using pip3
