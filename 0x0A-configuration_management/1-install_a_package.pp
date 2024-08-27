9f86d18af42cdfacceaa8d985be879f9ac139c38
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-27 04:32:45 -0700

    install package using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,27 @@
-# Install flask from pip3 using puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Python 3.8 using apt (for Debian/Ubuntu systems)
+package { 'python3.8':
+  ensure => 'installed',
+  provider => 'apt',
 }
+
+# Install pip3 (package manager for Python3)
+package { 'python3-pip':
+  ensure => 'installed',
+  provider => 'apt',
+  require => Package['python3.8'],
+}
+
+# Install Flask using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['Flask'],
+}
+
