d6492386f3d0798dac09faae195474f2d0a88fa9
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-08-27 05:44:37 +0000

    final fix of the puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-#fix bad "phpp" extentions to "php" in "wp-setting.php".
-{ 'fix-wordpress':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
-}
+#puppet scripts that fixes incorrect 'phpp' extensions to 'php'
 
+exec { 'fix-phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/usr/bin/:/bin/',
+}
